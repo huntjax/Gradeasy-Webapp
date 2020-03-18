8afc4c9f980762b5e7770f540c3d6d872bfcfed6
Hunter Pace <hpace5@uncc.edu>
2020-03-18 14:17:05 -0400

    added edit grades and showing students class grade

--- ai_folder/HelloWorld.py
+++ ai_folder/HelloWorld.py
@@ -11,7 +11,7 @@ path = sys.argv[1]
 #AI print what it reads
 
 #fake result for testing
-grade = ['a','a','a','a','a','test','test','test','test','test']
+grade = ['a','a','a','a','a','test','test','test','Wrong','Wrong']
 for i in grade:
     print(i)
 sys.stdout.flush
